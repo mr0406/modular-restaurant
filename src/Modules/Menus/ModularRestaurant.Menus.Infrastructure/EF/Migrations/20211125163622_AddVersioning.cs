0b1f67392676f4938f5574573ecbb12b9c4d7775
Marcin Rakowski <rakowski.marcin@outlook.com>
2021-11-25 18:37:51 +0100

    Add missing files

---
+++ src/Modules/Menus/ModularRestaurant.Menus.Infrastructure/EF/Migrations/20211125163622_AddVersioning.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModularRestaurant.Menus.Infrastructure.EF.Migrations
+{
+    public partial class AddVersioning : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                schema: "menus",
+                table: "Menus",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                schema: "menus",
+                table: "Menus");
+        }
+    }
+}
